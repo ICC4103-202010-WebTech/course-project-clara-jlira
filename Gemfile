e2f5513b5d249d947515877cce89ce5d9c06b6d6
CristobalLara <clara@miuandes.cl>
2020-06-22 01:43:34 -0400

    Devise

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class EmailValidator < ActiveModel::EachValidator
 end
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :memberships, dependent: :destroy
   has_many :invitations, dependent: :destroy
   has_many :events, dependent: :destroy
