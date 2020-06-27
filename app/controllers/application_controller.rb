d1ae0cc4bdc91cf79d90601e3b5f8c09b2a728aa
CristobalLara <clara@miuandes.cl>
2020-06-26 23:35:39 -0400

    Edit Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for()
-    current_user
+  def after_sign_in_path_for(resources)
+   current_user
   end
 
   def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
 
-  validates :user_name, :first_name, :last_name, :address, :password, presence: true
+  validates :user_name, :first_name, :last_name, :address, presence: true
   validates :user_name, uniqueness: true
   validates :email, presence: true, email: true
   validates :email, uniqueness: true
